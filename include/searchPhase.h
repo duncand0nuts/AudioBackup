bea965badc530fee878c873fea800660746885ff
Angus <angusmaidment@googlemail.com>
2015-03-30 17:52:36 +0100

    searchPhase class
    wrote header defining searchPhase class

---
+++ include/searchPhase.h
@@ -0,0 +1,17 @@
+/*
+The searchPhase class performs a recursive traversal of the target directory
+*/
+
+#ifndef __searchPhase__
+#define __searchPhase__
+
+class searchPhase
+{
+public:
+	searchPhase();  // constructor
+	~searchPhase();  // destructor
+private:
+};
+
+
+#endif
